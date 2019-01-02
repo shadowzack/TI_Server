3fb5f0a9795071ba6c0528a405487bf2198de29c
mahmod hasan <battleforge-war@hotmail.com>
2019-01-02 15:52:49 +0200

    second

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+//const db = require('./config/keys').mongoURI;
+const passport = require('passport');
+//const eventsRoutes = require('./API/routes/events');
+
+/*
+mongoose.connect(db)
+    .then(() => {
+        console.log('mongoDB connected');
+    }).catch((err) => {
+        console.log(err);
+    });
+*/
+//app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+//cors to allow access 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", " POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
+
+
+
+//routes
+//app.use('/users', usersRoutes);
+
+
+app.get('/',(req,res,next)=>{
+    res.json({
+        message:"check the api in the link bellow",
+        link:"https://events-social-network-api.herokuapp.com/"
+    });
+});
+
+//handling page not found error
+app.use((req, res, next) => {
+    const error = new Error('page not found');
+    error.status = 404;
+    next(error);
+
+});
+
+//handling rest of errors in case its not handeled in routes
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+
+
+module.exports = app;
