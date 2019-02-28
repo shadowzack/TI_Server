a9296b05a516b117b888fe97204f1c8d8aed003b
Adham Khalilieh <adham@megatam.net>
2019-02-28 02:08:13 +0200

    try

--- API/controllers/Tags.js
+++ API/controllers/Tags.js
@@ -1,46 +1,43 @@
-const Data_tags = require("../models/Data_tags");
-const mongoose = require("mongoose");
-const async = require("async");
-const { intersection } = require("underscore");
-const http = require("http");
-const request = require("request");
-const await =require("await");
-const cheerio = require("cheerio");
+const Data_tags = require('../models/Data_tags');
+const mongoose = require('mongoose');
+const async = require('async');
+const { intersection } = require('underscore');
+const http = require('http');
+const request = require('request');
+// const await =require("await");
+const cheerio = require('cheerio');
 
+const scrape = async (qIds, callback) => {
+	resArray = await [];
+	if (qIds.length < 10) max = qIds.length;
+	else max = 10;
+	for (let index = 0; index < max; index++) {
+		request(
+			{
+				method: 'GET',
+				url: `https://stackoverflow.com/questions/${qIds[index]}`
+			},
+			function(err, response, body, callback) {
+				if (err) return console.error(err);
 
+				// get the HTML body from WordThink.com
+				$ = cheerio.load(body);
 
+				var question = $('#question-header h1 a').text();
+				console.log(question);
+				resArray.push({ question: question, id: qIds[index] });
+			}
+		);
+	}
 
- const scrape = (qIds,callback) => {
-  resArray = [];
-  if (qIds.length < 10) max = qIds.length;
-  else max = 10;
-  for (let index = 0; index < max; index++) {
-    request(
-      {
-        method: "GET",
-        url: `https://stackoverflow.com/questions/${qIds[index]}`
-      },
-      function(err, response, body, callback) {
-        if (err) return console.error(err);
-
-        // get the HTML body from WordThink.com
-        $ = cheerio.load(body);
-
-        var question = $("#question-header h1 a").text();
-        console.log(question);
-        resArray.push({ question: question, id: qIds[index] });
-      }
-    );
-  }
-  //callback(resArray)
-  return resArray
-  //return resArray;
-  
+	//callback(resArray)
+	return resArray;
+	//return resArray;
 };
 
 exports.getLanaguageTags = (req, res, next) => {
-  Data_tags.find({ source: req.params.source }, { "year.tags.qIds": 0 })
-    /*Data_tags.aggregate([
+	Data_tags.find({ source: req.params.source }, { 'year.tags.qIds': 0 })
+		/*Data_tags.aggregate([
     { $match: { source: req.params.source } },
     { $unwind: "$year" },
     { $unwind: "$year.tags" },
@@ -55,120 +52,113 @@ exports.getLanaguageTags = (req, res, next) => {
       }
     }
   ])*/
-    .then(tags => {
-      res.json(tags);
-    })
-    .catch(err => res.status(500).json(err));
+		.then((tags) => {
+			res.json(tags);
+		})
+		.catch((err) => res.status(500).json(err));
 };
 exports.getLanaguageTagQustions = (req, res, next) => {
-  Data_tags.aggregate([
-    { $match: { source: req.params.source } },
-    { $unwind: "$year" },
-    { $unwind: "$year.tags" },
-    { $match: { "year.tags.tag": req.params.tag } }
-  ])
-    .then(tags => {
-      res.json(tags);
-    })
-    .catch(err => res.status(500).json(err));
+	Data_tags.aggregate([
+		{ $match: { source: req.params.source } },
+		{ $unwind: '$year' },
+		{ $unwind: '$year.tags' },
+		{ $match: { 'year.tags.tag': req.params.tag } }
+	])
+		.then((tags) => {
+			res.json(tags);
+		})
+		.catch((err) => res.status(500).json(err));
 };
 
 exports.getTagsQustions = (req, res, next) => {
-  Data_tags.find(
-    { source: req.params.source },
-    { "year.tags.qIds": { $slice: [10, 10] } }
-  )
-    .then(tags => {
-      res.json(tags);
-    })
-    .catch(err => res.status(500).json(err));
+	Data_tags.find({ source: req.params.source }, { 'year.tags.qIds': { $slice: [ 10, 10 ] } })
+		.then((tags) => {
+			res.json(tags);
+		})
+		.catch((err) => res.status(500).json(err));
 };
 
 exports.compareLanguagesByTags = (req, res, next) => {
-  const newComp = {
-    first: req.body.first,
-    second: req.body.second
-  };
-  async.parallel(
-    [
-      function(callback) {
-        Data_tags.aggregate(
-          [
-            { $match: { source: newComp.first.source } },
-            { $unwind: "$year" },
-            { $unwind: "$year.tags" },
-            { $match: { "year.tags.tag": newComp.first.tag } }
-          ],
-          callback
-        );
-      },
-      function(callback) {
-        Data_tags.aggregate(
-          [
-            { $match: { source: newComp.second.source } },
-            { $unwind: "$year" },
-            { $unwind: "$year.tags" },
-            { $match: { "year.tags.tag": newComp.second.tag } }
-          ],
-          callback
-        );
-      }
-    ],
+	const newComp = {
+		first: req.body.first,
+		second: req.body.second
+	};
 
-    (err, results) => {
-      const first = results[0];
-      const second = results[1];
+	async.parallel(
+		[
+			function(callback) {
+				Data_tags.aggregate(
+					[
+						{ $match: { source: newComp.first.source } },
+						{ $unwind: '$year' },
+						{ $unwind: '$year.tags' },
+						{ $match: { 'year.tags.tag': newComp.first.tag } }
+					],
+					callback
+				);
+			},
+			function(callback) {
+				Data_tags.aggregate(
+					[
+						{ $match: { source: newComp.second.source } },
+						{ $unwind: '$year' },
+						{ $unwind: '$year.tags' },
+						{ $match: { 'year.tags.tag': newComp.second.tag } }
+					],
+					callback
+				);
+			}
+		],
+		(err, results) => {
+			const first = results[0];
+			const second = results[1];
 
-      //intersection=_.intersection(firstArray,secondArray);
+			//intersection=_.intersection(firstArray,secondArray);
 
-      try {
-        firstArray = first[0].year.tags.qIds;
-        secondArray = second[0].year.tags.qIds;
-        //intersection:_.intersection(firstArray,secondArray)
+			try {
+				firstArray = first[0].year.tags.qIds;
+				secondArray = second[0].year.tags.qIds;
+				//intersection:_.intersection(firstArray,secondArray)
 
-        intersectionArray = [];
-        j = 0;
-        for (var i = 0; i < firstArray.length; ++i)
-          if (secondArray.indexOf(firstArray[i]) != -1)
-            intersectionArray[j++] = firstArray[i];
+				intersectionArray = [];
+				j = 0;
+				for (var i = 0; i < firstArray.length; ++i)
+					if (secondArray.indexOf(firstArray[i]) != -1) intersectionArray[j++] = firstArray[i];
 
-        if (err) {
-          throw err;
-        }
+				if (err) {
+					throw err;
+				}
 
-        
-          questionArray = scrape(intersectionArray);
-        
-        
-      
-        //JSON.parse(questionArray)
+				questionArray = scrape(intersectionArray);
 
-          res.json({
-          questionArray: questionArray,
-          intersectionArray: intersectionArray,
-          firstTag: {
-            source: first[0].source,
-            tag: first[0].year.tags.tag,
-            hits: first[0].year.tags.hits
-          },
-          secondTag: {
-            source: second[0].source,
-            tag: second[0].year.tags.tag,
-            hits: second[0].year.tags.hits
-          }
-        });
-        // res.json({first:first,second,second});
-        // res.json({ firstArray:firstArray,secondArray:secondArray });
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    }
-  );
+				//JSON.parse(questionArray)
+
+				res.json({
+					questionArray: questionArray,
+					intersectionArray: intersectionArray,
+					firstTag: {
+						source: first[0].source,
+						tag: first[0].year.tags.tag,
+						hits: first[0].year.tags.hits
+					},
+					secondTag: {
+						source: second[0].source,
+						tag: second[0].year.tags.tag,
+						hits: second[0].year.tags.hits
+					}
+				});
+				// res.json({first:first,second,second});
+				// res.json({ firstArray:firstArray,secondArray:secondArray });
+			} catch (err) {
+				res.status(500).json(err);
+			}
+		}
+	);
 };
 
 //keep it form me
 exports.getIntersection = (req, res, next) => {
-  /*Data_tags.aggregate([
+	/*Data_tags.aggregate([
     {
       $match: {
         source: { $in: [req.params.first, req.params.second] }
@@ -191,29 +181,29 @@ exports.getIntersection = (req, res, next) => {
     }
   ])*/
 
-  Data_tags.aggregate([
-    { $match: { source: { $in: [req.params.first, req.params.second] } } },
-    {
-      $group: {
-        _id: 0,
-        sets: { $push: "$year.tags" },
-        initialSet: { $first: "$year.tags" }
-      }
-    },
-    {
-      $project: {
-        commonSets: {
-          $reduce: {
-            input: "$sets",
-            initialValue: "$initialSet",
-            in: { $setIntersection: ["$$value", "$$this"] }
-          }
-        }
-      }
-    }
-  ])
-    .then(tags => {
-      res.json(tags);
-    })
-    .catch(err => res.status(500).json(err));
+	Data_tags.aggregate([
+		{ $match: { source: { $in: [ req.params.first, req.params.second ] } } },
+		{
+			$group: {
+				_id: 0,
+				sets: { $push: '$year.tags' },
+				initialSet: { $first: '$year.tags' }
+			}
+		},
+		{
+			$project: {
+				commonSets: {
+					$reduce: {
+						input: '$sets',
+						initialValue: '$initialSet',
+						in: { $setIntersection: [ '$$value', '$$this' ] }
+					}
+				}
+			}
+		}
+	])
+		.then((tags) => {
+			res.json(tags);
+		})
+		.catch((err) => res.status(500).json(err));
 };
